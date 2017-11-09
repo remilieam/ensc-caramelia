c71336d9a9402bb3565ef90e04e736c90a203804
Alexia Tartas <aletartas@ensc.fr>
2017-11-09 22:56:23 +0100

    Copie du projet au cas où la version nous fasse tout recommencer...

---
+++ CarAmelia 2 - Copie/Assets/Scripts/CarController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    // Le chemin
+    public Transform path;
+    private List<Transform> nodes;
+    private int currectNode = 0;
+
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+    public WheelCollider wheelRL;
+    public WheelCollider wheelRR;
+    public float maxMotorTorque = 80f;
+    public float maxBrakeTorque = 150f;
+    public float maxSteerAngle = 45f;
+    public float currentSpeed;
+    public float maxSpeed = 100f;
+    public Vector3 centerOfMass;
+    public bool isBraking = false;
+    public Texture2D textureNormal;
+    public Texture2D textureBraking;
+    public Renderer carRenderer;
+
+    public float test;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().centerOfMass = centerOfMass;
+
+        // On recrée notre liste de noeuds
+        Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+        for (int i = 0; i < pathTransforms.Length; i++)
+        {
+            if (pathTransforms[i] != path.transform)
+            {
+                nodes.Add(pathTransforms[i]);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        ApplySteer();
+        Drive();
+        CheckWaypointDistance();
+        Braking();
+    }
+
+    void ApplySteer()
+    {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currectNode].position);
+        //// Calcul que je n'ai pas compris
+        float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+        wheelFL.steerAngle = newSteer;
+        wheelFR.steerAngle = newSteer;
+    }
+
+    private void Drive()
+    {
+        // Vitesse
+        currentSpeed = 2 * Mathf.PI * wheelFL.radius * wheelFL.rpm * 60 / 1000;
+
+        if (currentSpeed < maxSpeed && !isBraking) {
+            wheelFL.motorTorque = maxMotorTorque;
+            wheelFR.motorTorque = maxMotorTorque;
+        } else {
+            wheelFL.motorTorque = 0;
+            wheelFR.motorTorque = 0;
+        }
+    }
+    // Permet d'actualiser le noeud
+    private void CheckWaypointDistance()
+    {
+        if (Vector3.Distance(transform.position, nodes[currectNode].position) < test)
+        {
+            //isBraking = true;            
+        //}
+        //else
+        //{
+        //    isBraking = false;
+            if (currectNode == nodes.Count - 1)
+            {
+                currectNode = 0;
+            }
+            else
+            {
+                currectNode++;
+            }
+        }
+    }
+
+    private void Braking()
+    {
+        if (isBraking)
+        {
+            carRenderer.material.mainTexture = textureBraking;
+            wheelRL.brakeTorque = maxBrakeTorque;
+            wheelRR.brakeTorque = maxBrakeTorque;
+        }
+        else
+        {
+            carRenderer.material.mainTexture = textureNormal;
+            wheelRL.brakeTorque = 0;
+            wheelRR.brakeTorque = 0;
+        }
+    }
+}
